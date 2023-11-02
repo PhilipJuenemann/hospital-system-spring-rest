941f1eec854b2f94d858e3f8e3a26535c9970500
Philip Jünemann <philip.juenemanns@gmail.com>
2023-11-02 09:59:04 +0100

    first commit

---
+++ src/main/java/com/hospitalsystemspringrest/hospitalsystem/HospitalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalsystemspringrest.hospitalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalsystemspringrest/hospitalsystem/HospitalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalsystemspringrest.hospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
